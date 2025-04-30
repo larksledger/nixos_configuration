0ff93e55e18d777fb75cefcad34b60d6b2b111a1
Larks Ledger <larksledger@gmail.com>
2025-04-30 11:37:11 +0200

    Fix symbolic links not working

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,135 @@
-/etc/nixos/configuration.nix
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 3;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+ services.xserver.videoDrivers = [ "nvidia"];
+ services.xserver.displayManager.gdm.enable = true;
+ services.xserver.displayManager.gdm.wayland = true;
+ 
+ hardware.nvidia.package = pkgs.linuxKernel.packages.linux_6_6.nvidia_x11;
+ hardware.nvidia.open = true;
+ hardware.nvidia.modesetting.enable = true;
+ programs.zsh.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.lark = {
+    isNormalUser = true;
+    description = "lark";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    shell = pkgs.zsh;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  programs.hyprland.enable = true;
+  programs.thunar.enable = true;
+  programs.xfconf.enable = true;
+  services.gvfs.enable = true;
+  services.tumbler.enable = true;
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  nixpkgs.config.cudaSupport = true;
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+     git
+     vim
+     kitty
+     firefox
+     vscodium
+     (vscode-with-extensions.override {
+       vscode = vscodium;
+       vscodeExtensions = with vscode-extensions; [
+         ms-python.python
+       ];
+     })
+    hyprpanel
+    zotero
+    blender
+    wofi
+    obs-studio
+    keepassxc
+  ];
+
+ fonts.packages = with pkgs; [
+     (nerdfonts.override { fonts = ["FiraCode"];})
+     fira-code
+ ]; 
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
--- flake.nix
+++ flake.nix
@@ -1,1 +1,24 @@
-/etc/nixos/flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11"; # this can be stable, but if it is do not make hyprpanel follow it
+    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux"; # change to whatever your system should be
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit system;
+        inherit inputs;
+      };
+      modules = [
+        {nixpkgs.overlays = [inputs.hyprpanel.overlay];}
+        ./configuration.nix
+      ];
+    };
+  };
+}

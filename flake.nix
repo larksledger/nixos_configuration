dda8c37dc72c2210d05fbed4b4c6cd643e678c07
Larks Ledger <larksledger@gmail.com>
2025-05-02 23:56:23 +0200

    Add stylix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11"; # this can be stable, but if it is do not make hyprpanel follow it
     hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
+    stylix.url = "github:danth/stylix/release-24.11";
   };
 
   outputs = inputs @ {
     nixpkgs,
+    stylix,
     ...
   }: let
     system = "x86_64-linux"; # change to whatever your system should be
@@ -17,6 +19,7 @@
       };
       modules = [
         {nixpkgs.overlays = [inputs.hyprpanel.overlay];}
+        stylix.nixosModules.stylix
         ./configuration.nix
       ];
     };
